ed6e5d9a106603b40a12b1eef6e53db25808cf87
omidpirhadi <atlasplants@gmail.com>
2021-11-14 10:37:42 +0330

    omid
    sound add

---
+++ Assets/Scripts/Alarts/Alart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Diaco.Alart
+{
+
+    public enum AlartType { Notification = 0, Dialog = 1, SmallPopup = 2 }
+
+    public class Alart : MonoBehaviour
+    {
+        public void AlartShow(AlartData alartData)
+        {
+            if (alartData.alartType == (int)AlartType.Notification)
+            {
+
+            }
+            else if (alartData.alartType == (int)AlartType.Dialog)
+            {
+
+            }
+            else if (alartData.alartType == (int)AlartType.SmallPopup)
+            {
+
+            }
+
+        }
+    }
+    [Serializable]
+    public struct AlartData
+    {
+        public int alartType;
+        public string[] images;
+        public string[] contexts;
+        public int buttonType;
+        public string command;
+    }
+
+}
