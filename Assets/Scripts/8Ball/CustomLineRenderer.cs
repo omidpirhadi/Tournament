87b475853586d79a59e888a6f1a4610f1cb33574
omidpirhadi <atlasplants@gmail.com>
2022-01-04 08:56:39 +0330

    omid
    add shapes lib

---
+++ Assets/Scripts/8Ball/CustomLineRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Shapes;
+public class CustomLineRenderer : MonoBehaviour
+{
+    [SerializeField] Line Fill, Stroke;
+    [SerializeField] Transform StartPosition,EndPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetPosition();
+    }
+
+    public void SetPosition()
+    {
+        Fill.Start = StartPosition.localPosition;
+        Stroke.Start  = StartPosition.localPosition;
+        Fill.End = EndPosition.localPosition;
+        Stroke.End = EndPosition.localPosition;
+    }
+}
