5b6f23fdb8a3fba555fa4412f0f1698c97fe69f6
omidpirhadi <atlasplants@gmail.com>
2022-06-27 23:14:17 +0430

    Update Test_UI.cs

--- Assets/Scripts/Utility/Test_UI.cs
+++ Assets/Scripts/Utility/Test_UI.cs
@@ -1,21 +1,109 @@
-﻿using System.Collections;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
-[ExecuteInEditMode]
+
 public class Test_UI : MonoBehaviour
 {
-    public ScrollRect scrollRect;
+   
 
     private void Start()
     {
-        scrollRect = GetComponent<ScrollRect>();
-        scrollRect.onValueChanged.AddListener(x => {
+        Aa();
+        Xx();
+        Cc();
+        
+    }
 
-            Debug.Log($"min H {scrollRect.minHeight} , min W{scrollRect.minWidth}, Perf w{ scrollRect.preferredWidth}, Perf H{ scrollRect.preferredHeight}");
+    public IEnumerator A()
+    {
+        int a = 0;
+        while (a<1000)
+        {
+            Debug.Log("A:"+a);
+            a++;
+        }
+        yield return null;
+    }
+    public IEnumerator B()
+    {
+        int a = 0;
+        while (a < 1000)
+        {
+            Debug.Log("B:" + a);
+            a++;
+        }
+        yield return null;
+    }
+    public IEnumerator C()
+    {
+        int a = 0;
+        while (a < 1000)
+        {
+            Debug.Log("C:" + a);
+            a++;
+        }
+        yield return null;
+    }
+    public void Aa()
+    {
+        Task.Run(() =>
+        {
+            int a = 0;
+            while (a < 1000)
+            {
+                Debug.Log("Aa:" + a);
+                a++;
+            }
         });
+        /// yield return null;
+    }
+    public async void Bb()
+    {
+        await Task.Run(() =>
+         {
+             int a = 0;
+             while (a < 1000)
+             {
+                 Debug.Log("Bb:" + a);
+                 a++;
+             }
+         });
+       // await Cc();
+       //// yield return null;
+    }
+    public  Task Cc()
+    {
+        Task task = Task.Run(() =>
+        {
+            int a = 0;
+            while (a < 1000)
+            {
+                Debug.Log("Cc:" + a);
+                a++;
+            }
+        });
+        return task;
 
+        /// yield return null;
+    }
+    public Task Xx()
+    {
+        Task task = Task.Run(() =>
+        {
+            int a = 0;
+            while (a < 1000)
+            {
+                Debug.Log("Xx:" + a);
+                a++;
+            }
+        });
+        return task;
+
+        /// yield return null;
     }
 }
